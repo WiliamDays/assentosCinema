b2c8b7c394822e30828e361737abed5e332621c7
Wiliam Pereira Dias <80425917+WiliamDays@users.noreply.github.com>
2022-10-07 07:05:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# assentosCinema
+ 
---
+++ main.js
@@ -0,0 +1,89 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let assento = document.getElementsByClassName("assento")
+assento.value = true
+oper 
+
+function preencherAssento(num){
+    if (assento.value == true){
+        assento.src = "./Imagens/acentoVermelho.jpg"
+        assento.value = false
+    }
+    else 
+        if(assento.value == false){
+        assento.src = "./Imagens/acentoAzul.png"
+        assento.value = true
+    }
+}*/
